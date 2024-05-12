25196e8c3153536673caf4780117fd931cd3d6a8
Kisa Santos <joao.neto.dos.santos@avanade.com>
2024-05-12 14:55:39 -0300

    Exercicio finalizado

--- TarefasApplication/Controllers/TarefaController.cs
+++ TarefasApplication/Controllers/TarefaController.cs
@@ -7,12 +7,17 @@ namespace TarefasApplication.Controllers
 {
     public class TarefaController : Controller
     {
-        private List<Tarefa> _tarefa = new List<Tarefa>()
+
+
+        private static List<Tarefa> _tarefa = new List<Tarefa>
         {
+            new Tarefa { TarefaId = 1, TarefaNome = "Tarefa 1", StatusTarefa = "Pendente", InicioTarefa = DateTime.Now },
+            new Tarefa { TarefaId = 2, TarefaNome = "Tarefa 2", StatusTarefa = "Pendente", InicioTarefa = DateTime.Now },
+            new Tarefa { TarefaId = 3, TarefaNome = "Tarefa 3", StatusTarefa = "Concluida", InicioTarefa = DateTime.Now, ConclusaoTarefa = DateTime.Now },
+            new Tarefa { TarefaId = 3, TarefaNome = "Tarefa 4", StatusTarefa = "Concluida", InicioTarefa = DateTime.Now, ConclusaoTarefa = DateTime.Now }
 
         };
 
-        private List<Tarefa> tarefaConcluida = new List<Tarefa>();
 
         public IActionResult Index()
         {
@@ -38,10 +43,20 @@ namespace TarefasApplication.Controllers
             
         }
 
+        public IActionResult Details(int id)
+        {
+            var tarefas = _tarefa.FirstOrDefault(t => t.TarefaId == id);
+            if (tarefas == null)
+            {
+                return NotFound();
+            }
+            return View(tarefas);
+        }
+
         public IActionResult Delete(int id)
         {
             var tarefa = _tarefa.FirstOrDefault(t => t.TarefaId == id);
-            if (tarefa != null) {
+            if (tarefa == null) {
                 return NotFound();
             }
 
@@ -60,18 +75,27 @@ namespace TarefasApplication.Controllers
             return View(tarefa);
         }
 
+
         [HttpPost]
         public IActionResult Edit(Tarefa tarefa)
         {
             if (ModelState.IsValid)
             {
                 var existingTarefa = _tarefa.FirstOrDefault(t => t.TarefaId == tarefa.TarefaId);
-                if (existingTarefa == null) 
+                if (existingTarefa != null) 
                 {
                     existingTarefa.TarefaNome = tarefa.TarefaNome;
                     existingTarefa.StatusTarefa = tarefa.StatusTarefa;
                     existingTarefa.InicioTarefa = tarefa.InicioTarefa;
                     existingTarefa.ConclusaoTarefa = tarefa.ConclusaoTarefa;
+                    if (tarefa.StatusTarefa.Equals("Concluida", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        existingTarefa.ConclusaoTarefa = DateTime.Now;
+                    }
+                    else
+                    {
+                        existingTarefa.ConclusaoTarefa = null;
+                    }
                 }
                 return RedirectToAction("Index");
             }
@@ -80,15 +104,14 @@ namespace TarefasApplication.Controllers
 
         public IActionResult Concluidas()
         {
-            foreach (var tarefa in _tarefa)
-            {
-                if (tarefa.StatusTarefa == true)
-                {
-                    tarefaConcluida.Add(tarefa);
-                    return RedirectToAction("Concluidas");
-                }
-            }
+            var tarefaConcluida = _tarefa.Where(t => t.StatusTarefa == "Concluida").ToList();
             return View(tarefaConcluida);
+        }
+
+        public IActionResult Pendente()
+        {
+            var tarefaPendente = _tarefa.Where(t => t.StatusTarefa == "Pendente").ToList();
+            return View(tarefaPendente);
         }
     }
 }
--- TarefasApplication/Models/Tarefa.cs
+++ TarefasApplication/Models/Tarefa.cs
@@ -1,11 +1,15 @@
-﻿namespace TarefasApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TarefasApplication.Models
 {
     public class Tarefa
     {
         public int TarefaId { get; set; }
+
+        [Required]
         public string? TarefaNome { get; set; }
-        public bool StatusTarefa { get; set; }
+        public string? StatusTarefa { get; set; }
         public DateTime? InicioTarefa { get; set; } = DateTime.Now;
-        public DateTime? ConclusaoTarefa { get; set; }
+        public DateTime? ConclusaoTarefa { get; set; } = null;
     }
 }
